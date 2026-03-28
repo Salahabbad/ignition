dfe620eab5e72efe3ce00272468ec555039d79cc
Salahabbad <105237317+Salahabbad@users.noreply.github.com>
2026-03-29 00:53:37 +0100

    Create amplifyClient.ts
    add cog1

---
+++ app/login/amplifyClient.ts
@@ -0,0 +1,9 @@
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    region: 'eu-west-1',
+    userPoolId: 'us-east-1_Dd8MiEw8D',
+    userPoolWebClientId: '3h8crv621qpst2r6i9j18im1l8',
+  },
+})
